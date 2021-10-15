fe13dbf309c474104d235c636ee0ebf0ab424555
ZeldoR <zeldor@ya.ru>
2021-10-15 11:50:58 +0300

    feat: add light style for edit button

--- scripts/script.js
+++ scripts/script.js
@@ -1,11 +1,13 @@
-// Cмена оформления
+// Change theme
 const headLogo = document.querySelector('.header__logo');
 const root = document.querySelector('.root');
+const editButton = document.querySelector('.profile__edit-button');
 const place = document.querySelectorAll('.place');
 
 function changeTheme() {
   root.classList.toggle('root_light');
   headLogo.classList.toggle('header__logo_light');
+  editButton.classList.toggle('profile__edit-button_light');
   place.forEach(element => {
     element.classList.toggle('place_light');
   });
@@ -15,14 +17,21 @@ headLogo.addEventListener('click', changeTheme);
 
 
 // Popup
-const addButton = document.querySelector('.profile__add-button');
 const popup = document.querySelector('.popup');
 const closeButton = document.querySelector('.popup__close-button');
 
-addButton.addEventListener('click', function(){
+editButton.addEventListener('click', function(){
   popup.classList.add('popup_opened');
 });
 
 closeButton.addEventListener('click', function(){
   popup.classList.remove('popup_opened');
 });
+
+
+// Add button
+const addButton = document.querySelector('.profile__add-button');
+function noFunctionalAlert() {
+  alert('Данный функционал пока не реализован..');
+}
+addButton.addEventListener('click', noFunctionalAlert);
