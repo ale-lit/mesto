3fc8a297147bf18783d5590904e2b16831919189
ZeldoR <zeldor@ya.ru>
2021-10-15 11:37:36 +0300

    feat: add open|close popup functional

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,4 @@
-// Добавляем смену оформления
+// Cмена оформления
 const headLogo = document.querySelector('.header__logo');
 const root = document.querySelector('.root');
 const place = document.querySelectorAll('.place');
@@ -6,8 +6,23 @@ const place = document.querySelectorAll('.place');
 function changeTheme() {
   root.classList.toggle('root_light');
   headLogo.classList.toggle('header__logo_light');
-  console.log(place);
-  //place.classList.toggle('place_light');
+  place.forEach(element => {
+    element.classList.toggle('place_light');
+  });
 }
 
 headLogo.addEventListener('click', changeTheme);
+
+
+// Popup
+const addButton = document.querySelector('.profile__add-button');
+const popup = document.querySelector('.popup');
+const closeButton = document.querySelector('.popup__close-button');
+
+addButton.addEventListener('click', function(){
+  popup.classList.add('popup_opened');
+});
+
+closeButton.addEventListener('click', function(){
+  popup.classList.remove('popup_opened');
+});
