81bd0132fe7aad925eaa409dbe005e38b525f02f
ZeldoR <zeldor@ya.ru>
2021-10-30 17:19:08 +0300

    feat: add functional for add new cards

--- scripts/index.js
+++ scripts/index.js
@@ -5,7 +5,7 @@
 const rootElement = document.querySelector('.root');
 const headLogoElement = rootElement.querySelector('.header__logo');
 const placesElement = document.querySelector('.places');
-const placeTemplate = document.querySelector('.place-template').content;
+const placeTemplate = document.querySelector('#place').content;
 const editButtonElement = rootElement.querySelector('.profile__edit-button');
 const currentNameElement = rootElement.querySelector('.profile__name');
 const currentSpecialityElement = rootElement.querySelector('.profile__speciality');
@@ -13,9 +13,11 @@ const addButtonElement = document.querySelector('.profile__add-button');
 // Popup Elements
 const popupElement = document.querySelector('.popup');
 const closeButtonElement = popupElement.querySelector('.popup__close-button');
+const popupTitle = popupElement.querySelector('.popup__title');
 const formElement = popupElement.querySelector('.popup__form');
-const inputNameElement = popupElement.querySelector('#input-name');
-const inputSpecialityElement = popupElement.querySelector('#input-speciality');
+const inputFirstElement = popupElement.querySelector('#input-first');
+const inputSecondElement = popupElement.querySelector('#input-second');
+const popupSaveButton = popupElement.querySelector('.popup__save-button');
 // Cards Array
 const initialCards = [
   {
@@ -31,6 +33,7 @@ const initialCards = [
     link: './images/places/sudak.jpg'
   },
   {
+
     name: 'Геленджик',
     link: './images/places/gelendjick.jpg'
   },
@@ -45,15 +48,24 @@ const initialCards = [
 ];
 
 // ************************************************************
+// *** EVENTS ***
+// ************************************************************
+headLogoElement.addEventListener('click', changeTheme);
+addButtonElement.addEventListener('click', () => openPopup('addNewPlace'));
+editButtonElement.addEventListener('click', () => openPopup('editProfile'));
+closeButtonElement.addEventListener('click', closePopup);
+popupElement.addEventListener('click', closePopupByClickOverlay);
+
+// ************************************************************
 // *** FUNCTIONS ***
 // ************************************************************
 // Add Cards On Load Page
-function addCardsOnload() {
+function addCardsOnloadPage() {
   initialCards.forEach((item) => {
     addCard(item.name, item.link);
   });
 }
-addCardsOnload();
+addCardsOnloadPage();
 
 // Add One Card
 function addCard(name, image) {
@@ -69,6 +81,67 @@ function addCard(name, image) {
   placesElement.prepend(cardElement);
 }
 
+// Open Popup
+function openPopup(action) {
+  if (action === 'editProfile') {
+    // Popup Title
+    popupTitle.textContent = 'Редактировать профиль';
+    // Popup First Input
+    inputFirstElement.value = currentNameElement.textContent;
+    inputFirstElement.placeholder = 'Имя';
+    // Popup Second Input
+    inputSecondElement.value = currentSpecialityElement.textContent;
+    inputSecondElement.placeholder = 'Специализация';
+    // Save Button
+    popupSaveButton.value = 'Сохранить';
+    // Event
+    formElement.addEventListener('submit', changeProfile);
+  } else if (action === 'addNewPlace') {
+    // Popup Title
+    popupTitle.textContent = 'Новое место';
+    // Popup First Input
+    inputFirstElement.value = '';
+    inputFirstElement.placeholder = 'Название';
+    // Popup Second Input
+    inputSecondElement.value = '';
+    inputSecondElement.placeholder = 'Ссылка на картинку';
+    // Save Button
+    popupSaveButton.value = 'Создать';
+    // Event
+    formElement.addEventListener('submit', addNewCard);
+  }
+
+  popupElement.classList.add('popup_opened');
+}
+// Close Popup
+function closePopup() {
+  popupElement.classList.remove('popup_opened');
+  clearListeners();
+}
+function closePopupByClickOverlay(event) {
+  if (event.target === event.currentTarget) closePopup();
+}
+
+// Change Profile
+function changeProfile(evt) {
+  evt.preventDefault();
+  currentNameElement.textContent = inputFirstElement.value;
+  currentSpecialityElement.textContent = inputSecondElement.value;
+  clearListeners();
+  closePopup();
+}
+// Add New Place
+function addNewCard(evt) {
+  evt.preventDefault();
+  addCard(inputFirstElement.value, inputSecondElement.value);
+  clearListeners();
+  closePopup();
+}
+// Clear All Listeners
+function clearListeners() {
+  formElement.removeEventListener('submit', changeProfile);
+  formElement.removeEventListener('submit', addNewCard);
+}
 // Change Theme
 function changeTheme() {
   const placeElement = rootElement.querySelectorAll('.place');
@@ -79,37 +152,3 @@ function changeTheme() {
     element.classList.toggle('place_theme_light');
   });
 }
-// Add Place (in progress)
-function noFunctionalAlert() {
-  alert('Данный функционал ещё не реализован :(');
-}
-// Open Popup
-function openPopup() {
-  inputNameElement.value = currentNameElement.textContent;
-  inputSpecialityElement.value = currentSpecialityElement.textContent;
-  popupElement.classList.add('popup_opened');
-}
-// Close Popup
-function closePopup() {
-  popupElement.classList.remove('popup_opened');
-}
-function closePopupByClickOverlay(event) {
-  if (event.target === event.currentTarget) closePopup();
-}
-// Change Profile Info
-function changeProfile(evt) {
-  evt.preventDefault();
-  currentNameElement.textContent = inputNameElement.value;
-  currentSpecialityElement.textContent = inputSpecialityElement.value;
-  closePopup();
-}
-
-// ************************************************************
-// *** EVENTS ***
-// ************************************************************
-headLogoElement.addEventListener('click', changeTheme);
-addButtonElement.addEventListener('click', noFunctionalAlert);
-editButtonElement.addEventListener('click', openPopup);
-closeButtonElement.addEventListener('click', closePopup);
-formElement.addEventListener('submit', changeProfile);
-popupElement.addEventListener('click', closePopupByClickOverlay);
